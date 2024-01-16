96ce283d2685aaf914199798c02998736de54b7a
ChristianTovar <tovarchristian21@gmail.com>
2024-01-16 11:16:24 -0500

    Add content

--- lib/poke_tab_web/live/live_dashboard/pokemon.ex
+++ lib/poke_tab_web/live/live_dashboard/pokemon.ex
@@ -4,13 +4,86 @@ defmodule PokeTabWeb.LiveDashboard.Pokemon do
 
   @impl true
   def menu_link(_, _) do
-    {:ok, "Pokémon"}
+    {:ok, "Pokédex"}
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    pokemon = fetch_random_pokemon()
+
+    {:ok, assign(socket, pokemon: pokemon)}
   end
 
   @impl true
   def render(assigns) do
     ~H"""
-    <div>Gotta catch 'em all!</div>
+    <.row>
+      <:col>
+        <.card title={"#{@pokemon.name} ##{@pokemon.id}"} hint="something">
+          <img src={@pokemon.image} alt={@pokemon.name} />
+        </.card>
+      </:col>
+      <:col>
+        <.row>
+          <:col>
+            <.card title="Weight">
+              <%= @pokemon.weight %>
+            </.card>
+          </:col>
+          <:col>
+            <.card title="Height">
+              <%= @pokemon.height %>
+            </.card>
+          </:col>
+          <:col>
+            <.card title="Base XP">
+              <%= @pokemon.base_xp %>
+            </.card>
+          </:col>
+        </.row>
+        <.row>
+          <:col>
+            <.fields_card title="Types" fields={@pokemon.types} />
+          </:col>
+        </.row>
+
+        <.row>
+          <:col>
+            <.card_title title="Game Sprites" />
+          </:col>
+        </.row>
+        <.row>
+          <:col>
+            <.card>
+              <img src={@pokemon.sprites["front_default"]} />
+            </.card>
+          </:col>
+          <:col>
+            <.card>
+              <img src={@pokemon.sprites["front_shiny"]} />
+            </.card>
+          </:col>
+        </.row>
+      </:col>
+    </.row>
     """
   end
+
+  defp fetch_random_pokemon do
+    body = Req.get!("https://pokeapi.co/api/v2/pokemon/#{Enum.random(1..1010)}").body
+
+    %{
+      name: String.capitalize(body["name"]),
+      id: body["id"],
+      weight: body["weight"],
+      height: body["height"],
+      base_xp: body["base_experience"] || "?",
+      types: list_types(body["types"]),
+      image: body["sprites"]["other"]["official-artwork"]["front_default"],
+      sprites: body["sprites"]
+    }
+  end
+
+  defp list_types(types),
+    do: Enum.map(types, fn %{"type" => %{"name" => name}} -> {nil, name} end)
 end
--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule PokeTab.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:req, "~> 0.4.0"}
     ]
   end
 
